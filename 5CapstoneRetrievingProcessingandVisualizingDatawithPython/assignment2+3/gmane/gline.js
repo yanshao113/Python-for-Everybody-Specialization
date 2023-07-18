1f13eeec85d06d4589db07d84ed636d5cf7444ee
yanshao113 <139802247+yanshao113@users.noreply.github.com>
2023-07-17 20:16:55 -0700

    Add files via upload

---
+++ 5CapstoneRetrievingProcessingandVisualizingDatawithPython/assignment2+3/gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','ufp.pt','indiana.edu','ucdavis.edu','berkeley.edu','columbia.edu','uct.ac.za','mac.com','virginia.edu','gmail.com'],
+['2005',57,10,12,11,12,13,14,12,9,10],
+['2006',54,24,20,17,14,11,10,9,12,10]
+];
